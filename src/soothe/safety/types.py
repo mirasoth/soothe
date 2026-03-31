fd113c8bb32a46cff35de05db9c469a8e44e822d
xiaming <chenxm35@gmail.com>
2026-03-31 20:30:51 +0800

    feat(safety): add security constants module

---
+++ src/soothe/safety/types.py
@@ -0,0 +1,17 @@
+"""Security-related types and constants."""
+
+from typing import Final
+
+# System directories that should never be used as workspace
+INVALID_WORKSPACE_DIRS: Final[frozenset[str]] = frozenset({
+    "/",
+    "/Users",
+    "/home",
+    "/System",
+    "/Library",
+    "/Applications",
+    "/usr",
+    "/var",
+    "/tmp",
+    "/etc",
+})
